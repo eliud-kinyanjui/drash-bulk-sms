087b0891f06ef1e110feabf8a713528f4fc28c8a
Ndirangu Waweru <4353067+nwaweru@users.noreply.github.com>
2022-10-14 14:51:43 +0300

    cleanup

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,8 +4,16 @@
     <div class="container">
         <div class="row vh-100 align-items-center">
             <div class="col-12 text-center">
-                <h1 >Welcome to {{ $page.props.env.APP_NAME }}</h1>
+                <h1>Welcome to {{ env.APP_NAME }}</h1>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        env: Object
+    }
+}
+</script>
