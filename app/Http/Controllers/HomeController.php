8cd75b6bdfa90e7d4448413d75e1d0860ab8e49e
Ndirangu Waweru <4353067+nwaweru@users.noreply.github.com>
2022-10-22 19:11:06 +0300

    added system summary to home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Home');
+        $contactGroups = Auth::user()->contactGroups()->count();
+        $messages = Auth::user()->messages()->count();
+
+        return Inertia::render('Home', [
+            'stats' => [
+                'contactGroups' => $contactGroups,
+                'messages' => $messages,
+            ],
+        ]);
     }
 }
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,15 +2,44 @@
     <Head title="Home" />
 
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
+        <div class="row mt-5">
+            <div class="col-md-12 text-center">
+                <h1>
+                    {{ auth.user.name }}, <br>
+                    welcome!
+                </h1>
+            </div>
+        </div>
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-3">
+                <div class="card border border-dark">
                     <div class="card-body">
-                        You are logged in!
+                        <div class="text-center">
+                            Contact Groups <br>
+                            <span class="display-6">{{ stats.contactGroups }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card border border-dark">
+                    <div class="card-body">
+                        <div class="text-center">
+                            Messages <br>
+                            <span class="display-6">{{ stats.messages }}</span>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        auth: Object,
+        stats: Object,
+    }
+}
+</script>
