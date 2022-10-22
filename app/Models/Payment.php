6b1cd34856e195faf2fb6385d1f14a6d5cc94f04
Ndirangu Waweru <4353067+nwaweru@users.noreply.github.com>
2022-10-22 21:40:17 +0300

    refactored

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Payment extends Model
 {
@@ -21,12 +22,15 @@ class Payment extends Model
         'date',
     ];
 
-    protected $casts = [
-        'created_at' => 'datetime:d-m-Y @ h:i A',
-    ];
-
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::parse($value)->format('d-m-Y @ h:i A'),
+        );
     }
 }
