218c6f3f819d0432c0eb7caa23f5c16feb37677c
Ndirangu Waweru <4353067+nwaweru@users.noreply.github.com>
2022-10-22 22:33:52 +0300

    payments view now using datatables

--- resources/js/Pages/Payments/Index.vue
+++ resources/js/Pages/Payments/Index.vue
@@ -8,8 +8,16 @@
                 <div v-if="flash.status" class="alert alert-dismissible fade show text-center" :class="flash.status.type" role="alert">
                     {{ flash.status.message }}
                 </div>
-
-                <table v-if="payments.length" class="table">
+                <DataTable
+                    :data="tableData"
+                    :options="{
+                         'columnDefs': [{
+                            'targets': [1, 4],
+                            'orderable': false,
+                        }]
+                    }"
+                    class="table"
+                >
                     <thead>
                         <tr>
                             <th scope="col">#</th>
@@ -18,36 +26,52 @@
                             <th scope="col">Amount</th>
                             <th scope="col">
                                 <Link :href="route('payments.create')" class="btn btn-sm btn-primary">
-                                    <i class="fa-sharp fa-solid fa-plus"></i>
-                                    New Payment
+                                <i class="fa-sharp fa-solid fa-plus"></i>
+                                New Payment
                                 </Link>
                             </th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr v-for="(payment, index) in payments" :key="payment.id">
-                            <th scope="row">{{ ++index }}</th>
-                            <td>{{ payment.receipt }}</td>
-                            <td>{{ payment.phone}}</td>
-                            <td>KES {{ payment.amount}}</td>
-                            <td>Paid {{ payment.created_at }}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                </DataTable>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import DataTable from 'datatables.net-vue3';
+import DataTableBs5 from 'datatables.net-bs5';
+
+DataTable.use(DataTableBs5);
+
 export default {
+    components: {
+        DataTable,
+    },
+
     props: {
         auth: Object,
         payments: Object,
         flash: Object,
     },
 
+    data() {
+        return {
+            tableData: [],
+        };
+    },
+
     created() {
+        this.payments.forEach((value, index) => {
+            this.tableData.push([
+                ++index,
+                value.receipt,
+                value.phone,
+                value.amount,
+                value.created_at,
+            ]);
+        });
+
         Echo.private(`users.${this.auth.user.uuid}`)
             .listen('MpesaCallbackSaved', (e) => {
                 let payment = e.payment;
@@ -62,3 +86,8 @@ export default {
     }
 };
 </script>
+
+<style>
+@import 'bootstrap';
+@import 'datatables.net-bs5';
+</style>
