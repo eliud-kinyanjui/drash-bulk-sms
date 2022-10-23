04b562bdfeec7732fd2bc7533cb37de649a57a6d
Ndirangu Waweru <4353067+nwaweru@users.noreply.github.com>
2022-10-23 11:38:25 +0300

    format credit attribute to 2 decimal places

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -45,5 +46,12 @@ class User extends Authenticatable implements MustVerifyEmail
     public function payments()
     {
         return $this->hasMany(Payment::class);
+    }
+
+    protected function credit(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => number_format($value, 2),
+        );
     }
 }
