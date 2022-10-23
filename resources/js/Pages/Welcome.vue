a22291e594d66ba744f9f0b413dc5cbec04ec0a6
Ndirangu Waweru <4353067+nwaweru@users.noreply.github.com>
2022-10-23 23:38:20 +0300

    updated welcome text

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,10 +2,10 @@
     <Head title="Welcome" />
 
     <div class="px-4 py-5 my-5 text-center">
-        <h1 class="display-2 fw-bold mb-4">Bulk SMS Simplified.</h1>
+        <h1 class="display-2 fw-bold mb-4">Bulk SMS, Simplified.</h1>
         <div class="col-lg-6 mx-auto">
             <p class="lead mb-4">
-                Simply create your contact group and add some contacts,
+                Simply create your contact group(s) and add some contacts,
                 <br>
                 load your credit and send your SMS!
             </p>
